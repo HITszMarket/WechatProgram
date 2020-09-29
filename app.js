b9d2924672b7fba3c28eff6bb6bc7ed78b409ad4
Kiss-Moon <61782559+Kiss-Moon@users.noreply.github.com>
2020-09-29 10:35:04 +0800

    个人中心

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-
-
     // 登录
     wx.login({
       success: res => {
@@ -20,6 +17,7 @@ App({
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
+            lang: "zh_CN",
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
