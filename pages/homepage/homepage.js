e62a273da2f0c8459c340e0ab5c8169a00a8af99
Kiss-Moon <61782559+Kiss-Moon@users.noreply.github.com>
2020-10-12 09:03:59 +0800

    部分未完成

--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -2,10 +2,6 @@
 const app = getApp()
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     motto: "说点什么呢？长按修改吧！",
     userInfo: {},
@@ -15,61 +11,6 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
   //事件处理函数
   bindViewTap: function () {
     wx.navigateTo({
@@ -119,8 +60,32 @@ Page({
     })
   },
   changeMottoEnd: function (e) {
+    console.log(e.detail.value)
+    var motto = this.data.motto;
+    motto = e.detail.value;
     this.setData({
+      motto: motto,
       dis_motto: true
+    })
+  },
+  community:function(){
+    wx.navigateTo({
+      url: '../community/community',
+    })
+  },
+  toAdvise:function(e){
+    wx.navigateTo({
+      url: '../advise/advise',
+    })
+  },
+  toComplain:function(){
+    wx.navigateTo({
+      url: '../complain/complain',
+    })
+  },
+  toSet:function(){
+    wx.navigateTo({
+      url: '../set/set',
     })
   },
   checkLogin:function(){
